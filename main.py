22e335d44d273847c020320a99b1cfb89f4d5542
Abigail D. <10st2ooart@gmail.com>
2025-01-17 05:14:44 +0000

    I added multiple functions and changed a couple such as the name and age input functions and how they're worded, then the chatbot name. I then added a menu list to choose form and made a function for user_choice to work. (Only for option number 4.

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-print("Welcome to the Writing Promts Chatbot!")
+print("** Welcome to the Convenience Store Chatbot! **")
+print("   -----------------------------------------")
 name = input("What might your name be? ")
 
-age = input("Hello, " + name + "! How old are you? ")
+age = input("And how old are you? ")
+
+print("Good day " + name + ", It's a pleasure to meet you!")
+print("")
+print("Here are some options you may choose from: ")
+print("1. Store Hours")
+print("2. Store Locations")
+print("3. Avaliable Items")
+print("4. Exit Chatbot")
+
+user_choice = int(input("Now, what may I help you with " + name + "? "))
+
+if user_choice == 4:
+    print("Very well. See you later!")
